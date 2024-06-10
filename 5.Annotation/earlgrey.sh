b7f97f73be46c9088acb6dad00cf64b32929816b
Haley Carter <hsharoncarter@gmail.com>
2024-06-10 15:06:29 -0500

    create earlgrey.sh

---
+++ 5.Annotation/earlgrey.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo /home/exouser/Software/EarlGrey-4.2.4/earlGrey -g HiC/yahs/output_examples/yahs_pass6_scaffolds_final.fa -o ./earlGreyOutputs -s populusTrichocarpa -d yes -t 60
+
+
