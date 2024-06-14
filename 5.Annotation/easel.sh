5bb1707df28991933b4f478548d20ba7892de9b2
Haley Carter <hsharoncarter@gmail.com>
2024-06-14 18:23:30 -0500

    update easel.sh

--- 5.Annotation/easel.sh
+++ 5.Annotation/easel.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-sudo /home/exouser/Software/nextflow run -hub gitlab PlantGenomicsLab/easel \
-    -c nextflow.conf \
-    --genome annotation/earlGreyOutputsScaffolds/populusTrichocarpa_EarlGrey/populusTrichocarpa_summaryFiles/populusTrichocarpa.softmasked.fasta \
-    --user_reads annotation/rna-seq/*{1,2}.fastq.gz \
-    --busco_lineage embryophyta \
-    --order Viridiplantae \
-    --taxon null \
-    --training_set plant \
-    --outdir annotation/easel/
-    --with-report --with-trace
+./nextflow run -hub gitlab PlantGenomicsLab/easel \
+  -c nextflow.conf \
+  --genome /mnt/class_data/tempAnnotation/populusTrichocarpa.softmasked.singleContigShort.fasta \
+  --bam /mnt/class_data/5.Annotation/data/rna-seq/sorted.bam \
+  --busco_lineage embryophyta \
+  --order Viridiplantae     --taxon null \
+  --training_set plant \
+  --outdir /mnt/class_data/haley_outputs \
+  -w /mnt/class_data/haley_work \
+  --singularity_cache_dir /mnt/class_data/singularityForNextflow
