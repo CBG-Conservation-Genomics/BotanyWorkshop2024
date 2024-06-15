3e675938435452f4f5b5d7e10c3dd89207b9b4da
PlantBeast <152332277+PlantBeast@users.noreply.github.com>
2024-06-15 07:27:14 -0500

    Update run_orthofinder.sh

--- 6.ComparativeGenomics/run_orthofinder.sh
+++ 6.ComparativeGenomics/run_orthofinder.sh
@@ -2,9 +2,6 @@
 
 #script to run Orthofinder on Salicaeae
 
-cd
-git clone https://github.com/CBG-Conservation-Genomics/BotanyWorkshop2024.git
 mkdir Orthofinder
-ln -s /mnt/class_data/ .
 
-orthofinder -t 5 -M msa -S diamond -A mafft -T fasttree -f /mnt/class_data/ -o Orthofinder
+orthofinder -t 5 -M msa -A mafft -T fasttree -f /mnt/class_data/6.CompGen/OrthoFinder/ -o Orthofinder
