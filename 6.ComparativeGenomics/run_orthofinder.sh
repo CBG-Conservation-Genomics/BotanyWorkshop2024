db182dfbfaf623f7f18d0f2703239dfaabb48067
PlantBeast <152332277+PlantBeast@users.noreply.github.com>
2024-06-16 11:35:01 -0500

    Update run_orthofinder.sh

--- 6.ComparativeGenomics/run_orthofinder.sh
+++ 6.ComparativeGenomics/run_orthofinder.sh
@@ -6,4 +6,4 @@ mkdir Orthofinder
 cd Orthofinder
 ln -s /mnt/class_data/6.CompGen/OrthoFinder/*.fasta .
 
-orthofinder -t 5 -M msa -A mafft -T fasttree -f . -o Orthofinder
+/home/exouser/Software/OrthoFinder/orthofinder -t 5 -M msa -A mafft -T fasttree -f . -o Orthofinder
