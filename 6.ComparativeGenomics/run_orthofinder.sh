e4380f0620f7a811cf2efff20e8341f3626e4d21
PlantBeast <152332277+PlantBeast@users.noreply.github.com>
2024-06-15 20:51:26 -0500

    Update run_orthofinder.sh

--- 6.ComparativeGenomics/run_orthofinder.sh
+++ 6.ComparativeGenomics/run_orthofinder.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 #script to run Orthofinder on Salicaeae
-
+cd
 mkdir Orthofinder
+cd Orthofinder
+ln -s /mnt/class_data/6.CompGen/Orthofinder/* .
 
-orthofinder -t 5 -M msa -A mafft -T fasttree -f /mnt/class_data/6.CompGen/OrthoFinder/ -o Orthofinder
+orthofinder -t 5 -M msa -A mafft -T fasttree -f . -o Orthofinder
