74d5635257cf83fe5fca2de9395fd1642b6d87c8
PlantBeast <152332277+PlantBeast@users.noreply.github.com>
2024-06-16 10:57:42 -0500

    Update run_MCScan.sh

--- 6.ComparativeGenomics/run_MCScan.sh
+++ 6.ComparativeGenomics/run_MCScan.sh
@@ -9,22 +9,22 @@ cd MCScan
 ln -s /mnt/class_data/6.CompGen/MCScan/* .
 
 #convert gff annotations to bed
-python -m jcvi.formats.gff bed --type=CDS --key=Name Populus_deltoides.gff -o Populus_deltoides.bed
-python -m jcvi.formats.gff bed --type=mRNA --key=Name Populus_trichocarpa.gff -o Populus_trichocarpa.bed
+python3 -m jcvi.formats.gff bed --type=CDS --key=Name Populus_deltoides.gff -o Populus_deltoides.bed
+python3 -m jcvi.formats.gff bed --type=mRNA --key=Name Populus_trichocarpa.gff -o Populus_trichocarpa.bed
 
 #reformat fasta files
-python -m jcvi.formats.fasta format Populus_deltoides_CDS.fasta Populus_deltoides.cds
-python -m jcvi.formats.fasta format Populus_trichocarpa_CDS.fasta Populus_trichocarpa.cds
+python3 -m jcvi.formats.fasta format Populus_deltoides_CDS.fasta Populus_deltoides.cds
+python3 -m jcvi.formats.fasta format Populus_trichocarpa_CDS.fasta Populus_trichocarpa.cds
 
 #may need to remove excessive isoforms in some annotations, we aren't doing this here
 
 #do alignments, will run awhile
-python -m jcvi.compara.catalog ortholog Populus_deltoides Populus_trichocarpa --no_strip_names
+python3 -m jcvi.compara.catalog ortholog Populus_deltoides Populus_trichocarpa --no_strip_names
 
 #make synteny plot
-python -m jcvi.graphics.dotplot Populus_deltoides.Populus_trichocarpa.anchors
+python3 -m jcvi.graphics.dotplot Populus_deltoides.Populus_trichocarpa.anchors
 
 #view macrosynteny
-python -m jcvi.compara.synteny screen --minspan=30 --simple Populus_deltoides.Populus_trichocarpa.anchors Populus_deltoides.Populus_trichocarpa.anchors.new 
-python -m jcvi.graphics.karyotype seqids layout
+python3 -m jcvi.compara.synteny screen --minspan=30 --simple Populus_deltoides.Populus_trichocarpa.anchors Populus_deltoides.Populus_trichocarpa.anchors.new 
+python3 -m jcvi.graphics.karyotype seqids layout
 
