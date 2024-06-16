39684eea20624d2d8ccc9097639122f19bd2c317
PlantBeast <152332277+PlantBeast@users.noreply.github.com>
2024-06-15 20:16:18 -0500

    Update run_MCScan.sh

--- 6.ComparativeGenomics/run_MCScan.sh
+++ 6.ComparativeGenomics/run_MCScan.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 pip install jcvi
+sudo apt install last-align
 
 cd
 mkdir MCScan
@@ -12,18 +13,18 @@ python -m jcvi.formats.gff bed --type=CDS --key=Name Populus_deltoides.gff -o Po
 python -m jcvi.formats.gff bed --type=mRNA --key=Name Populus_trichocarpa.gff -o Populus_trichocarpa.bed
 
 #reformat fasta files
-python -m jcvi.formats.fasta format Populus_deltoides_CDS.fasta Populus_deloides.cds
+python -m jcvi.formats.fasta format Populus_deltoides_CDS.fasta Populus_deltoides.cds
 python -m jcvi.formats.fasta format Populus_trichocarpa_CDS.fasta Populus_trichocarpa.cds
 
 #may need to remove excessive isoforms in some annotations, we aren't doing this here
 
-#do alignments
-python -m jcvi.compara.catoalog ortholog Populus_deltoides Populus_trichocarpa --no_strip_names
+#do alignments, will run awhile
+python -m jcvi.compara.catalog ortholog Populus_deltoides Populus_trichocarpa --no_strip_names
 
 #make synteny plot
 python -m jcvi.graphics.dotplot Populus_deltoides.Populus_trichocarpa.anchors
 
 #view macrosynteny
-python -m jcvi.compara.synteny screen --minspan=30 --simple Populus_deltoides.Populus_trichocarpa.anchors grape.peach.anchors.new 
+python -m jcvi.compara.synteny screen --minspan=30 --simple Populus_deltoides.Populus_trichocarpa.anchors Populus_deltoides.Populus_trichocarpa.anchors.new 
 python -m jcvi.graphics.karyotype seqids layout
 
