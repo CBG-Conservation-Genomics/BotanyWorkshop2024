440802857179c15814589b47a52168c58605b4fb
Haley Carter <hsharoncarter@gmail.com>
2024-06-17 10:02:15 -0400

    final update to easel.sh

--- 5.Annotation/easel.sh
+++ 5.Annotation/easel.sh
@@ -2,10 +2,11 @@
 ./nextflow run -hub gitlab PlantGenomicsLab/easel \
   -c nextflow.conf \
   --genome /mnt/class_data/tempAnnotation/populusTrichocarpa.softmasked.singleContigShort.fasta \
-  --bam /mnt/class_data/5.Annotation/data/rna-seq/sorted.bam \
+  --bam /mnt/class_data/tempAnnotation/sorted.bam \
   --busco_lineage embryophyta \
   --order Viridiplantae     --taxon null \
   --training_set plant \
-  --outdir /mnt/class_data/haley_outputs \
-  -w /mnt/class_data/haley_work \
-  --singularity_cache_dir /mnt/class_data/singularityForNextflow
+  --outdir /mnt/class_data/easel_outputs \
+  -w /mnt/class_data/work \
+  --singularity_cache_dir /mnt/class_data/singularityForNextflow \
+  -profile docker
